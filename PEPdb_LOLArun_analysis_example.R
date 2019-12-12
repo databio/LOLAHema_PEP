ef2a1dcdf68c5b76de9f739197a8d41ba406fc3e
Jose E. Verdezoto Mosquera <josev4@uw.edu>
2019-12-12 10:57:42 -0500

    update LOLArun analysis script

--- PEPdb_LOLArun_analysis_example.R
+++ PEPdb_LOLArun_analysis_example.R
@@ -3,8 +3,11 @@ library(GenomicRanges)
 library(data.table)
 library("simpleCache")
 
-pep_object50 = loadPEPdb_CACHE("/project/shefflab/resources/regions/LOLAHema/LOLAHema_PEP","/LOLAHema_originalBED_config.yaml")
+# use loadPEPdb_CACHE to load 50 files from the LOLAHema_originalBED PEP database
+# loadPEPdb_CACHE takes 2 arguments: config folder path and config.yaml name 
+pepObject50 = loadPEPdb_CACHE("/project/shefflab/resources/regions/LOLAHema/LOLAHema_PEP","/LOLAHema_originalBED_config.yaml")
 
+# load query region sets from LOLA vignette
 regionSetA = readBed("~/lola_vignette_data/setA_100.bed")
 regionSetB = readBed("~/lola_vignette_data/setB_100.bed")
 regionSetC = readBed("~/lola_vignette_data/setC_100.bed")
@@ -12,5 +15,8 @@ activeDHS = readBed("~/lola_vignette_data/activeDHS_universe.bed")
 
 userSets = GRangesList(regionSetA, regionSetB, regionSetC)
 
-#runLOLA with originalBED LOLAHema (50 files)
-locResults_originalBED_PEP = runLOLA(userSets, activeDHS, pep_object50, cores=1)
+# run LOLA with the loaded PEP 
+locResultsOriginalBED = runLOLA(userSets, activeDHS, pepObject50, cores=1)
+
+# order results according to meanRnk
+locResultsOriginalBED[order(meanRnk, decreasing=FALSE),][1:20,]
